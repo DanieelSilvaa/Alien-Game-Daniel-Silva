902a904686b9bb6c972f514d7904c6032f39f3c2
DanieelSilvaa <89879391+DanieelSilvaa@users.noreply.github.com>
2021-09-12 22:09:11 -0500

    Initial commit

---
+++ Assets/Script/Animal.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float vida;
+    [SerializeField] GameManager gm;
+    float minX, maxX;
+    [SerializeField] bool movingRight;
+        
+   
+
+    float duracion = 0;
+    float powerRate = 3;
+
+    bool cantPoder = true;
+    float cantPower = 0;
+    bool poder= false ;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 esquinaInfDer = Camera.main.ViewportToWorldPoint(new Vector2(1, 0));
+        Vector2 esquinaInfIzq = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+            
+        maxX = esquinaInfDer.x;
+        minX = esquinaInfIzq.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingRight)
+        {
+            transform.Translate(new Vector2(speed * Time.deltaTime, 0), Space.World);
+        }
+        else
+        {
+            transform.Translate(new Vector2(-speed * Time.deltaTime, 0), Space.World);
+        }
+        if(transform.position.x > maxX)
+        {
+            movingRight = false;
+        }
+        else if(transform.position.x < minX)
+        {
+            movingRight = true;
+        }
+
+        if(cantPoder == true)
+        {
+            Power();
+        } else
+        {
+            Time.timeScale = 1;
+        }
+            
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject colision = collision.gameObject;
+        if (colision.tag == "Bullet")
+        {
+            if (poder == true)
+            {
+                vida = 1;
+            }
+            vida--;
+            if (vida == 0)
+            {
+                gm.ReducirNumEnemigos();
+                Destroy(this.gameObject);
+            }
+           
+        }
+    }
+     void Power()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && Time.unscaledTime >= duracion && poder == false)
+        {
+            poder = true;
+            Time.timeScale = 0.5f;
+            duracion = Time.unscaledTime + powerRate;
+            cantPower++;
+            if(cantPower > 3)
+            {
+                cantPoder = false;
+            }
+
+            
+        }
+        if (duracion <= Time.unscaledTime && poder == true)
+        {
+            Time.timeScale = 1;
+            poder = false;
+        }
+
+
+
+
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+  
+    [SerializeField] GameObject canvas;
+    [SerializeField] GameObject buttonNext;
+    [SerializeField] Spaceship player;
+    [SerializeField] int cantEn;
+
+    Animal animal;
+    bool finish = false;
+    bool gameP = false;
+    
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        canvas.SetActive(false);
+        buttonNext.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && finish == false)
+        {
+            PauseGame();
+        }
+        
+    }
+    public void StartGame()
+    {
+       
+        SceneManager.LoadScene(1);
+       
+
+
+    }
+    void PauseGame()
+    {
+        gameP = gameP ? false : true;
+        canvas.SetActive(gameP);
+        player.gamePaused = gameP;
+        Time.timeScale = gameP ? 0 : 1;
+    }
+   public void ReducirNumEnemigos()
+    {
+        cantEn --;
+        if (cantEn < 1)
+        {
+            Win();
+            
+        }
+    }
+    void Win()
+    {
+
+        buttonNext.SetActive(true);
+        player.gamePaused = true;
+        finish =! finish;
+        Time.timeScale = 0;
+      
+    }
+    public void NextLevel(int level)
+    {
+
+       
+        player.gamePaused = false;
+        Time.timeScale = 1;
+
+        SceneManager.LoadScene(level);
+    }
+}
